bafc7d0c74e981eda03a53c5cd07202ee6424d75
onatancoimbra <nsilvac1930@gmail.com>
2025-04-07 23:15:12 -0300

    Project complete

---
+++ src/assets/imageMap.js
@@ -0,0 +1,91 @@
+import morango from './acai/morango.avif';
+import banana from './acai/banana.avif';
+import leiteEmPo from './acai/ninho.avif';
+import pacoca from './acai/pacoquita.avif';
+import mel from './acai/mel.avif';
+import kiwi from './acai/kiwi.avif';
+import uva from './acai/uva.avif';
+import achocolatadoEmPo from './acai/achocolatadoempó.avif';
+import amendoim from './acai/amendoim.avif';
+import chocolateOuroBranco from './acai/chocolateourobranco.avif';
+import chocolateSonhoDeValsa from './acai/chocolatesonhodevalsa.avif';
+import copoAcai from './acai/copo.avif';
+import granola from './acai/granola.avif';
+import granulado from './acai/granulado.avif';
+import marmita from './acai/marmita.avif';
+import massaAcai from './acai/massaacai.avif';
+import massaCupuacu from './acai/massacupuacu.avif';
+import massaMista from './acai/massamista.avif';
+import ovomaltineRocks from './acai/ovomaltinerocks.avif';
+import sucrilhos from './acai/sucrilhos.avif';
+
+import pacoquitaBatida from './batida/pacoquita.avif';
+
+import chantily from './frutadafelicidade/chantily.avif';
+import copoFrutada from './frutadafelicidade/copo.avif';
+import kiwiFrutada from './frutadafelicidade/kiwi.avif';
+import ovomaltineFrutada from './frutadafelicidade/ovomaltinerocks.avif';
+import ovomaltine from './batida/ovomaltine.avif';
+import uvaFrutada from './frutadafelicidade/uva.avif';
+
+import bananaTapioca from './tapioca/banana.avif';
+import cremeDeAvela from './tapioca/cremedeavela.avif';
+import cremeDeLeitinho from './tapioca/cremedeleitinho.avif';
+import goma from './tapioca/goma.avif';
+import leiteCondensado from './tapioca/leitecondensado.avif';
+import manteigaAviacao from './tapioca/manteigaaviacao.avif';
+import morangoTapioca from './tapioca/morango.avif';
+import mussarela from './tapioca/mussarela.avif';
+import oregano from './tapioca/oregano.avif';
+import ouroBranco from './tapioca/ourobranco.avif';
+import peitoDeFrango from './tapioca/peitodefrango.avif';
+import peitoDeFrangoComMussarela from './tapioca/peitodefrangocommussarela.avif';
+import queijoPrato from './tapioca/queijoprato.avif';
+import sonhoDeValsa from './tapioca/sonhodevalsa.avif';
+
+export const imageMap = {
+  imgMorango: morango,
+  imgBanana: banana,
+  imgLeiteEmPo: leiteEmPo,
+  imgPacoca: pacoca,
+  imgMel: mel,
+  imgKiwi: kiwi,
+  imgUva: uva,
+  imgAchocolatadoEmPo: achocolatadoEmPo,
+  imgAmendoim: amendoim,
+  imgChocolateOuroBranco: chocolateOuroBranco,
+  imgChocolateSonhoDeValsa: chocolateSonhoDeValsa,
+  imgCopoAcai: copoAcai,
+  imgGranola: granola,
+  imgGranulado: granulado,
+  imgMarmita: marmita,
+  imgMassaAcai: massaAcai,
+  imgMassaCupuacu: massaCupuacu,
+  imgMassaMista: massaMista,
+  imgOvomaltineRocks: ovomaltineRocks,
+  imgSucrilhos: sucrilhos,
+
+  imgPacoquitaBatida: pacoquitaBatida,
+
+  imgChantily: chantily,
+  imgCopoFrutada: copoFrutada,
+  imgKiwiFrutada: kiwiFrutada,
+  imgOvomaltineFrutada: ovomaltineFrutada,
+  imgUvaFrutada: uvaFrutada,
+
+  imgBananaTapioca: bananaTapioca,
+  imgCremeDeAvela: cremeDeAvela,
+  imgCremeDeLeitinho: cremeDeLeitinho,
+  imgGoma: goma,
+  imgLeiteCondensado: leiteCondensado,
+  imgManteigaAviacao: manteigaAviacao,
+  imgMorangoTapioca: morangoTapioca,
+  imgMussarela: mussarela,
+  imgOregano: oregano,
+  imgOuroBranco: ouroBranco,
+  imgPeitoDeFrango: peitoDeFrango,
+  imgPeitoDeFrangoComMussarela: peitoDeFrangoComMussarela,
+  imgQueijoPrato: queijoPrato,
+  imgSonhoDeValsa: sonhoDeValsa,
+  imgOvomaltine: ovomaltine,
+};
